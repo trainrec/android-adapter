011d18bdb50f450841127e72e4ec5bcf06e09687
Eirik Jensen <eiriktj@pm.me>
2020-07-22 02:41:14 +0200

    Copy presenter from android-ui.

---
+++ src/main/java/no/trainrec/android/presenter/Presenter.kt
@@ -0,0 +1,27 @@
+package no.trainrec.android.ui
+
+import no.trainrec.android.framework.Storage
+
+import no.trainrec.core.use_case.EntryAdder
+import no.trainrec.core.domain.ExerciseEntry
+import no.trainrec.core.data.TrainingRecord
+
+class Presenter(appFilesDir: String) {
+    private val storage = Storage(appFilesDir)
+    private val record = TrainingRecord(storage)
+    private val entryAdder = EntryAdder(record)
+
+    fun addEntry(exerciseName: String) {
+        entryAdder.addEntry(exerciseName)
+    }
+
+    fun listEntries(): List<String> {
+        var formattedEntries = ArrayList<String>()
+        for (exerciseEntry in record.listEntries()) {
+            val exerciseDate = exerciseEntry.getDate()
+            val exerciseName = exerciseEntry.getExercise()
+            formattedEntries.add("$exerciseDate: $exerciseName")
+        }
+        return formattedEntries
+    }
+}
